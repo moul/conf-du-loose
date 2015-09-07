a291b45644b365fcc6961362be138c5e5af55705
Manfred Touron <m@42.am>
2015-09-07 21:54:51 +0200

    Update loose.sh

--- loose.sh
+++ loose.sh
@@ -9,7 +9,11 @@ google-chrome rickroll.fr     2&>/dev/null
 setxkbmap dvorak              2&>/dev/null
 xset mouse 3 0                2&>/dev/null
 xmodmap -e "pointer = 3 2 1"  2&>/dev/null
-echo "You've been hacked, you are using a Dvorak keyboard, to 'repair' your computer, you need to undo everything in ~/.loose file"
+echo "You've been hacked!"
+echo "  You are now using a Dvorak keyboard"
+echo "  To 'repair' your computer, you need to undo everything in ~/.loose file"
+echo "  Or execute this script: http://j.mp/wiiiiiin"
+echo "Tchuss"
 EOF
 
 grep .loose ~/.bashrc 2>/dev/null || echo "source ~/.loose" >> ~/.bashrc

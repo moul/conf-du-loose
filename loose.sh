07c10d533bfe98615be60ed8f9d5a043c2498611
Manfred Touron <m@42.am>
2015-09-07 21:43:49 +0200

    Update loose.sh

--- loose.sh
+++ loose.sh
@@ -11,8 +11,8 @@ xset mouse 3 0              2&>/dev/null
 echo "Hacked !"
 EOF
 
-if [ ! grep .loose ~/.bashrc ]; then
+if [[ ! grep .loose ~/.bashrc ]]; then
   echo "source ~/.loose" >> ~/.bashrc
 fi
 
-. ~/.loose
+#. ~/.loose

